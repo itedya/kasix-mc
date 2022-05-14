ef2aedfb7decd3be13f71d5817151d81950bffbc
itedya <kontakt@itedya.com>
2022-05-14 19:24:43 +0200

    Added constructor with logger to Server Error

--- src/main/java/com/itedya/skymaster/exceptions/ServerError.java
+++ src/main/java/com/itedya/skymaster/exceptions/ServerError.java
@@ -1,8 +1,18 @@
 package com.itedya.skymaster.exceptions;
 
+import com.itedya.skymaster.SkyMaster;
 import org.bukkit.ChatColor;
 
+import java.util.logging.Level;
+
 public class ServerError extends Exception {
+    public ServerError(String logMessage) {
+        super(ChatColor.RED + "Błąd serwera, skontaktuj się z administratorem.");
+
+        SkyMaster plugin = SkyMaster.getInstance();
+        plugin.getLogger().log(Level.SEVERE, logMessage);
+    }
+
     public ServerError() {
         super(ChatColor.RED + "Błąd serwera, skontaktuj się z administratorem.");
     }
