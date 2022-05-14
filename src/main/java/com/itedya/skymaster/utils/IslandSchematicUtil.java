24e5dad187f406aa7b0bbe12b6ae257aad34b9e8
itedya <kontakt@itedya.com>
2022-05-14 19:28:43 +0200

    Converted Island Schematic utility to new Island Schematic schema

--- src/main/java/com/itedya/skymaster/utils/IslandSchematicUtil.java
+++ src/main/java/com/itedya/skymaster/utils/IslandSchematicUtil.java
@@ -2,13 +2,13 @@ package com.itedya.skymaster.utils;
 
 import com.itedya.skymaster.daos.IslandSchematicDao;
 import com.itedya.skymaster.dtos.IslandSchematicDto;
+import com.itedya.skymaster.exceptions.ServerError;
 import org.bukkit.entity.Player;
 
-import java.io.IOException;
 import java.util.List;
 
 public class IslandSchematicUtil {
-    public static List<IslandSchematicDto> getWithPermission(Player player) throws IOException {
+    public static List<IslandSchematicDto> getWithPermission(Player player) throws ServerError {
         IslandSchematicDao dao = IslandSchematicDao.getInstance();
 
         return dao.getAll()
@@ -17,7 +17,7 @@ public class IslandSchematicUtil {
                 .toList();
     }
 
-    public static IslandSchematicDto getById(String id) throws IOException {
+    public static IslandSchematicDto getById(int id) throws ServerError {
         IslandSchematicDao dao = IslandSchematicDao.getInstance();
 
         return dao.getById(id);
