364de85766652e7bc788f3a285915bee6b682c60
itedya <kontakt@itedya.com>
2022-05-20 14:15:57 +0200

    Updated PersistentDataContainerUtil
    - added setString func
    - converted name of "add" to setInt func
    - added getString func
    - added getInt func

--- src/main/java/com/itedya/skymaster/utils/PersistentDataContainerUtil.java
+++ src/main/java/com/itedya/skymaster/utils/PersistentDataContainerUtil.java
@@ -6,9 +6,31 @@ import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
 
 public class PersistentDataContainerUtil {
-    public static void add(PersistentDataContainer container, String namespace, int value) {
+    public static void setString(PersistentDataContainer container, String namespace, String value) {
+        NamespacedKey key = new NamespacedKey(SkyMaster.getInstance(), namespace);
+
+        container.set(key, PersistentDataType.STRING, value);
+    }
+
+    public static void setInt(PersistentDataContainer container, String namespace, int value) {
         NamespacedKey key = new NamespacedKey(SkyMaster.getInstance(), namespace);
 
         container.set(key, PersistentDataType.INTEGER, value);
+    }
+
+    public static String getString(PersistentDataContainer container, String namespace) {
+        NamespacedKey key = new NamespacedKey(SkyMaster.getInstance(), namespace);
+
+        return container.get(key, PersistentDataType.STRING);
+    }
+
+    public static Integer getInt(PersistentDataContainer container, String namespace) {
+        NamespacedKey key = new NamespacedKey(SkyMaster.getInstance(), namespace);
+
+        try {
+            return container.get(key, PersistentDataType.INTEGER);
+        } catch (NullPointerException e) {
+            return null;
+        }
     }
 }
