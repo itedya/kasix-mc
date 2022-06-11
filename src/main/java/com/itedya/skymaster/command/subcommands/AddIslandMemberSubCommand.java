3af08201a5fcfaca3fbd6b2208e859bc20add821
itedya <kontakt@itedya.com>
2022-06-11 11:06:38 +0200

    Fixed Add Island Member Command

--- src/main/java/com/itedya/skymaster/command/subcommands/AddIslandMemberSubCommand.java
+++ src/main/java/com/itedya/skymaster/command/subcommands/AddIslandMemberSubCommand.java
@@ -1,27 +1,15 @@
 package com.itedya.skymaster.command.subcommands;
 
-import com.itedya.skymaster.SkyMaster;
-import com.itedya.skymaster.daos.Database;
-import com.itedya.skymaster.daos.IslandDao;
 import com.itedya.skymaster.daos.IslandInviteDao;
-import com.itedya.skymaster.dtos.IslandDto;
-import com.itedya.skymaster.utils.IslandUtil;
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.NamespacedKey;
+import com.itedya.skymaster.runnables.ShowIslandsForInvitesGuiRunnable;
+import com.itedya.skymaster.utils.ThreadUtil;
+import org.bukkit.*;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.persistence.PersistentDataType;
 import org.jetbrains.annotations.NotNull;
 
-import java.sql.Connection;
-import java.util.List;
-
-// todo: optimize this file
 public class AddIslandMemberSubCommand implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
@@ -43,6 +31,7 @@ public class AddIslandMemberSubCommand implements CommandExecutor {
 
             if (args[0].equals(player.getName())) {
                 player.sendMessage(ChatColor.YELLOW + "Nie możesz zaprosić sam siebie!");
+                return true;
             }
 
             Player invitedPlayer = Bukkit.getPlayer(args[0]);
@@ -57,40 +46,11 @@ public class AddIslandMemberSubCommand implements CommandExecutor {
                 return true;
             }
 
-            Inventory inventory = Bukkit.createInventory(null, 9, "Wybierz wyspę do której chcesz zaprosić");
-
-            Connection connection = Database.getInstance().getConnection();
-
-            IslandDao islandDao = new IslandDao(connection);
-            List<IslandDto> userIslands = islandDao.getByOwnerUuid(player.getUniqueId().toString());
-
-            IslandUtil.convertIslandDtosToItemStacks(userIslands)
-                    .stream()
-                    .forEach(ele -> {
-                        ItemMeta itemMeta = ele.getItemMeta();
-                        itemMeta.getPersistentDataContainer().set(
-                                new NamespacedKey(SkyMaster.getInstance(), "invite_from_player_uuid"),
-                                PersistentDataType.STRING,
-                                player.getUniqueId().toString()
-                        );
-
-                        itemMeta.getPersistentDataContainer().set(
-                                new NamespacedKey(SkyMaster.getInstance(), "invite_to_player_uuid"),
-                                PersistentDataType.STRING,
-                                invitedPlayer.getUniqueId().toString()
-                        );
-                        ele.setItemMeta(itemMeta);
-
-                        inventory.addItem(ele);
-                    });
-
-            player.openInventory(inventory);
-
-            return true;
+            ThreadUtil.async(new ShowIslandsForInvitesGuiRunnable(player, invitedPlayer));
         } catch (Exception e) {
             e.printStackTrace();
             sender.sendMessage(ChatColor.RED + "Server error.");
-            return true;
         }
+        return true;
     }
 }
