786211dd946731eb6f3a9700c67b66b76d8d8043
itedya <kontakt@itedya.com>
2022-06-11 10:35:41 +0200

    Fixed IslandHomeDao
    - reorganised "firstByIslandId" and "create" to throw SQLException instead of ServerError

--- src/main/java/com/itedya/skymaster/daos/IslandHomeDao.java
+++ src/main/java/com/itedya/skymaster/daos/IslandHomeDao.java
@@ -4,10 +4,7 @@ import com.itedya.skymaster.SkyMaster;
 import com.itedya.skymaster.dtos.IslandHomeDto;
 import com.itedya.skymaster.exceptions.ServerError;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.logging.Level;
 
 public class IslandHomeDao {
@@ -17,77 +14,65 @@ public class IslandHomeDao {
         this.connection = connection;
     }
 
-    public IslandHomeDto firstByIslandId(int id) throws ServerError {
+    public IslandHomeDto firstByIslandId(int id) throws SQLException {
         return firstByIslandId(id, false);
     }
 
-    public IslandHomeDto firstByIslandId(int id, Boolean withDeleted) throws ServerError {
-        SkyMaster plugin = SkyMaster.getInstance();
-
+    public IslandHomeDto firstByIslandId(int id, Boolean withDeleted) throws SQLException {
         String query = "SELECT skymaster_homes.* FROM skymaster_islands " +
                 "JOIN skymaster_island_has_homes ON skymaster_islands.id = skymaster_island_has_homes.islandId " +
                 "JOIN skymaster_homes ON skymaster_island_has_homes.homeId = skymaster_homes.id " +
                 "WHERE skymaster_islands.id = ?";
-        if (!withDeleted) query += " AND skymaster_island_has_homes.deletedAt IS NULL;";
+        if (!withDeleted) query += " AND skymaster_island_has_homes.deletedAt IS NULL";
         query += " LIMIT 1";
 
-        try {
-            PreparedStatement stmt = connection.prepareStatement(query);
+        IslandHomeDto result = null;
+        PreparedStatement stmt = connection.prepareStatement(query);
 
-            stmt.setInt(1, id);
+        stmt.setInt(1, id);
 
-            ResultSet rs = stmt.executeQuery();
+        ResultSet rs = stmt.executeQuery();
 
-            stmt.close();
+        if (rs.next()) result = new IslandHomeDto(rs);
 
-            if (rs.next()) {
-                rs.close();
-                return new IslandHomeDto(rs);
-            }
+        rs.close();
+        stmt.close();
 
-            rs.close();
-            return null;
-        } catch (SQLException e) {
-            plugin.getLogger().log(Level.SEVERE, "Database error", e);
-            throw new ServerError();
-        }
+        return result;
     }
 
-    public IslandHomeDto create(int islandId, IslandHomeDto islandHomeDto) throws ServerError {
-        SkyMaster plugin = SkyMaster.getInstance();
-
+    public IslandHomeDto create(int islandId, IslandHomeDto islandHomeDto) throws SQLException {
         String query = "INSERT INTO `skymaster_homes` SET x = ?, y = ?, z = ?, worldUuid = ?";
         String relationQuery = "INSERT INTO `skymaster_island_has_homes` SET islandId = ?, homeId = ?;";
 
-        try {
-            PreparedStatement stmt = connection.prepareStatement(query);
+        PreparedStatement stmt = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
 
-            stmt.setInt(1, islandHomeDto.getX());
-            stmt.setInt(2, islandHomeDto.getY());
-            stmt.setInt(3, islandHomeDto.getZ());
-            stmt.setString(4, islandHomeDto.getWorldUuid());
+        stmt.setInt(1, islandHomeDto.getX());
+        stmt.setInt(2, islandHomeDto.getY());
+        stmt.setInt(3, islandHomeDto.getZ());
+        stmt.setString(4, islandHomeDto.getWorldUuid());
 
-            int affectedRows = stmt.executeUpdate();
-            if (affectedRows == 0) throw new SQLException("No rows affected!");
+        int affectedRows = stmt.executeUpdate();
+        if (affectedRows == 0) throw new SQLException("No rows affected!");
 
-            ResultSet generatedKeys = stmt.getGeneratedKeys();
+        ResultSet generatedKeys = stmt.getGeneratedKeys();
 
-            if (generatedKeys.next()) islandHomeDto.setId(generatedKeys.getInt(1));
-            else throw new SQLException("No id generated for added home!");
+        if (generatedKeys.next()) islandHomeDto.setId(generatedKeys.getInt(1));
+        else throw new SQLException("No id generated for added home!");
 
-            stmt.close();
+        generatedKeys.close();
+        stmt.close();
 
-            stmt = connection.prepareStatement(relationQuery);
-            stmt.setInt(1, islandId);
-            stmt.setInt(2, islandHomeDto.getId());
+        stmt = connection.prepareStatement(relationQuery);
 
-            stmt.close();
+        stmt.setInt(1, islandId);
+        stmt.setInt(2, islandHomeDto.getId());
 
-            return islandHomeDto;
-        } catch (SQLException e) {
-            plugin.getLogger().log(Level.SEVERE, "Database error", e);
-            throw new ServerError();
-        }
+        stmt.executeUpdate();
+
+        stmt.close();
+
+        return islandHomeDto;
     }
 
     public void updateByIslandId(int islandId, IslandHomeDto islandHomeDto) throws ServerError {
