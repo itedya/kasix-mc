8d2b3db6f642a0a406bfa45fc8c4688f8f948328
itedya <kontakt@itedya.com>
2022-07-15 19:04:38 +0200

    Added Player to onEvent GUIHandler function

--- src/main/java/com/itedya/skymaster/guihandlers/GUIHandler.java
+++ src/main/java/com/itedya/skymaster/guihandlers/GUIHandler.java
@@ -1,8 +1,9 @@
 package com.itedya.skymaster.guihandlers;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
 
 public interface GUIHandler {
-    default void onEvent(InventoryClickEvent event) {
+    default void onEvent(InventoryClickEvent event, Player player) {
     }
 }
