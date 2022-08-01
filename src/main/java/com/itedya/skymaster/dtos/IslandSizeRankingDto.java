ebaa341d4eab585a7d66f9ced7d911fb3c085a28
itedya <bdomanski@protonmail.com>
2022-08-01 20:11:11 +0200

    Redone SkyMasterPlaceholderExpansion

---
+++ src/main/java/com/itedya/skymaster/dtos/IslandSizeRankingDto.java
@@ -0,0 +1,8 @@
+package com.itedya.skymaster.dtos;
+
+import com.itedya.skymaster.dtos.database.IslandDto;
+
+public class IslandSizeRankingDto {
+    public IslandDto islandDto;
+    public String islandOwnerName;
+}
--- src/main/java/com/itedya/skymaster/rankings/RankingManager.java
+++ src/main/java/com/itedya/skymaster/rankings/RankingManager.java
@@ -1,15 +1,21 @@
 package com.itedya.skymaster.rankings;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
-public abstract class RankingManager {
-    protected Map data = new HashMap();
+public abstract class RankingManager<T> {
+    private List<T> data;
 
-    public void refresh() {
+    public abstract void refresh();
+
+    public void saveData(List<T> data) {
+        this.data = data;
     }
 
-    public Map getDataForPlace(int place) {
-        return null;
+    public T getDataForPlace(int place) {
+        try {
+            return data.get(place - 1);
+        } catch (Exception e) {
+            return null;
+        }
     }
 }
