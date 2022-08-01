d04a94f02ea32ce700a0b5d4c8048b7e9b3a3eaa
itedya <bdomanski@protonmail.com>
2022-08-02 01:50:35 +0200

    Made cost of expanding island calculate itself

--- src/main/java/com/itedya/skymaster/runnables/expand/ExpandIslandGuiRunnable.java
+++ src/main/java/com/itedya/skymaster/runnables/expand/ExpandIslandGuiRunnable.java
@@ -6,6 +6,7 @@ import com.itedya.skymaster.daos.IslandDao;
 import com.itedya.skymaster.dtos.database.IslandDto;
 import com.itedya.skymaster.runnables.SkymasterRunnable;
 import com.itedya.skymaster.runnables.island.ResetWorldGuardPermissionsRunnable;
+import com.itedya.skymaster.utils.IslandUtil;
 import com.itedya.skymaster.utils.ThreadUtil;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -17,13 +18,20 @@ import java.util.Objects;
 import java.util.UUID;
 
 public class ExpandIslandGuiRunnable extends SkymasterRunnable {
+    private Player executor;
+    private int islandId;
+    private int newIslandRadius;
+    private boolean admin;
+    private IslandDto islandDto;
+    private OfflinePlayer owner;
+
     public ExpandIslandGuiRunnable(Player executor, int islandId, int newIslandRadius, boolean admin) {
         super(executor, true);
 
-        data.put("executor", executor);
-        data.put("islandId", islandId);
-        data.put("newRadius", newIslandRadius);
-        data.put("admin", admin);
+        this.executor = executor;
+        this.islandId = islandId;
+        this.newIslandRadius = newIslandRadius;
+        this.admin = admin;
     }
 
     private Connection connection;
@@ -35,23 +43,18 @@ public class ExpandIslandGuiRunnable extends SkymasterRunnable {
 
             IslandDao islandDao = new IslandDao(connection);
 
-            var islandId = (int) data.get("islandId");
-            var dto = islandDao.getById(islandId);
-            if (dto == null) {
-                var executor = (Player) data.get("executor");
+            islandDto = islandDao.getById(islandId);
+            if (islandDto == null) {
                 executor.sendRawMessage(ChatColor.RED + "Hmm... serwer mówi że stoisz na wyspie, która jest usunięta. To błąd, zgłoś do administracji.");
                 return;
             }
 
-            var executor = (Player) data.get("executor");
-            var admin = (boolean) data.get("admin");
-            if (!admin && !Objects.equals(dto.ownerUuid, executor.getUniqueId().toString())) {
+            if (!admin && !Objects.equals(islandDto.ownerUuid, executor.getUniqueId().toString())) {
                 executor.sendMessage(ChatColor.YELLOW + "Ta wyspa nie jest twoja!");
                 return;
             }
 
-            dto.radius = (int) data.get("newRadius");
-            data.put("islandDto", dto);
+            islandDto.radius = newIslandRadius;
 
 
             if (!admin) {
@@ -66,22 +69,20 @@ public class ExpandIslandGuiRunnable extends SkymasterRunnable {
 
     public void checkBalance() {
         try {
-            var dto = (IslandDto) data.get("islandDto");
-
-            var owner = Bukkit.getOfflinePlayer(UUID.fromString(dto.ownerUuid));
-            data.put("islandOwner", owner);
+            owner = Bukkit.getOfflinePlayer(UUID.fromString(islandDto.ownerUuid));
 
             var eco = SkyMaster.getEconomy();
             var bal = eco.getBalance(owner);
 
-            if (bal < 100) {
-                var executor = (Player) data.get("executor");
-                executor.sendRawMessage(ChatColor.YELLOW + "Potrzebujesz 100$, żeby powiększyć wyspę.");
+            var cost = IslandUtil.getExpandCost(newIslandRadius);
+
+            if (bal < cost) {
+                executor.sendRawMessage(ChatColor.YELLOW + "Potrzebujesz " + cost + ", żeby powiększyć wyspę.");
                 super.closeDatabase();
                 return;
             }
 
-            eco.withdrawPlayer(owner, 100);
+            eco.withdrawPlayer(owner, cost);
 
             ThreadUtil.async(this::saveIntoDatabase);
         } catch (Exception e) {
@@ -91,8 +92,6 @@ public class ExpandIslandGuiRunnable extends SkymasterRunnable {
 
     public void saveIntoDatabase() {
         try {
-            var islandDto = (IslandDto) data.get("islandDto");
-
             IslandDao islandDao = new IslandDao(connection);
             islandDao.update(islandDto);
 
@@ -102,34 +101,26 @@ public class ExpandIslandGuiRunnable extends SkymasterRunnable {
             ThreadUtil.sync(this::modifyBalance);
         } catch (Exception e) {
             super.errorHandling(e);
-
-            var executor = (Player) data.get("executor");
-            var islandDto = (IslandDto) data.get("islandDto");
             ThreadUtil.async(new ResetWorldGuardPermissionsRunnable(executor, islandDto.id));
         }
     }
 
     public void modifyBalance() {
         try {
-            var admin = (boolean) data.get("admin");
-            var executor = (Player) data.get("executor");
-            var dto = (IslandDto) data.get("islandDto");
-            var owner = (OfflinePlayer) data.get("islandOwner");
-
-            ThreadUtil.async(new ResetWorldGuardPermissionsRunnable(executor, dto.id));
+            ThreadUtil.async(new ResetWorldGuardPermissionsRunnable(executor, islandDto.id));
 
             if (admin) {
-                owner = Bukkit.getOfflinePlayer(UUID.fromString(dto.ownerUuid));
+                owner = Bukkit.getOfflinePlayer(UUID.fromString(islandDto.ownerUuid));
 
                 executor.sendRawMessage("%sPowiększyłeś wyspę %s\"%s\"%s gracza %s do %s kratek!".formatted(
-                        ChatColor.GREEN, ChatColor.BOLD, dto.name, ChatColor.RESET + "" + ChatColor.GREEN,
+                        ChatColor.GREEN, ChatColor.BOLD, islandDto.name, ChatColor.RESET + "" + ChatColor.GREEN,
                         ChatColor.BOLD + owner.getName() + ChatColor.RESET + "" + ChatColor.GREEN,
-                        ChatColor.BOLD + "" + (dto.radius * 2) + "" + ChatColor.RESET + "" + ChatColor.GREEN
+                        ChatColor.BOLD + "" + (islandDto.radius * 2) + "" + ChatColor.RESET + "" + ChatColor.GREEN
                 ));
             } else {
                 executor.sendRawMessage("%sPowiększyłeś wyspę %s\"%s\"%s do %s kratek!".formatted(
-                        ChatColor.GREEN, ChatColor.BOLD, dto.name, ChatColor.RESET + "" + ChatColor.GREEN,
-                        ChatColor.BOLD + "" + (dto.radius * 2) + "" + ChatColor.RESET + "" + ChatColor.GREEN
+                        ChatColor.GREEN, ChatColor.BOLD, islandDto.name, ChatColor.RESET + "" + ChatColor.GREEN,
+                        ChatColor.BOLD + "" + (islandDto.radius * 2) + "" + ChatColor.RESET + "" + ChatColor.GREEN
                 ));
             }
         } catch (Exception e) {
