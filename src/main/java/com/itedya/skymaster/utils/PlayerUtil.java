9a5f1e129debbd5537b8cc8b7b52b870a386f745
itedya <bdomanski@protonmail.com>
2022-08-12 19:26:44 +0200

    Added documentation and not null decorator to getMaxAllowedIslandMembers func in PlayerUtil

--- src/main/java/com/itedya/skymaster/utils/PlayerUtil.java
+++ src/main/java/com/itedya/skymaster/utils/PlayerUtil.java
@@ -3,6 +3,7 @@ package com.itedya.skymaster.utils;
 import com.itedya.skymaster.SkyMaster;
 import org.bukkit.entity.Player;
 import org.bukkit.permissions.PermissionAttachmentInfo;
+import org.jetbrains.annotations.NotNull;
 
 public class PlayerUtil {
     public static Integer getMaxAllowedIslands(Player player) {
@@ -44,7 +45,14 @@ public class PlayerUtil {
     }
 
     // TODO: USE IT
-    public static Integer getMaxAllowedIslandMembers(Player player) {
+
+    /**
+     * Checks how much island members can user have
+     *
+     * @param player Player to check
+     * @return max allowed island members in Integer object
+     */
+    public static @NotNull Integer getMaxAllowedIslandMembers(Player player) {
         for (PermissionAttachmentInfo permission : player.getEffectivePermissions()) {
             String permissionId = permission.getPermission();
             if (permissionId.startsWith("skymaster.islands.max-members.")) {
@@ -55,7 +63,7 @@ public class PlayerUtil {
                 } catch (Exception e) {
                     SkyMaster.getPluginLogger().severe("Wrong format in max island members permission.");
                     e.printStackTrace();
-                    return null;
+                    return 2;
                 }
             }
         }
