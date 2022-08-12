e2330c948626e7581eb54cda12d3817bfd90dfa8
itedya <bdomanski@protonmail.com>
2022-08-12 20:18:45 +0200

    Removed unnecessary functions from ConfigUtil
    - removed getColouredString (string) func
    - removed getColouredString (string, string) func

--- src/main/java/com/itedya/skymaster/utils/ConfigUtil.java
+++ src/main/java/com/itedya/skymaster/utils/ConfigUtil.java
@@ -1,7 +1,6 @@
 package com.itedya.skymaster.utils;
 
 import com.itedya.skymaster.SkyMaster;
-import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
 
 public class ConfigUtil {
@@ -9,23 +8,5 @@ public class ConfigUtil {
         SkyMaster plugin = SkyMaster.getInstance();
 
         return plugin.getConfig();
-    }
-
-    public static String getColouredString(String id) {
-        FileConfiguration configuration = getConfig();
-        String rawMessage = configuration.getString(id);
-
-        if (rawMessage == null) return null;
-
-        return ChatColor.translateAlternateColorCodes('&', rawMessage);
-    }
-
-    public static String getColouredString(String id, String defaultMessage) {
-        String configMessage = getColouredString(id);
-        if (configMessage == null) {
-            return ChatColor.translateAlternateColorCodes('&', defaultMessage);
-        }
-
-        return configMessage;
     }
 }
