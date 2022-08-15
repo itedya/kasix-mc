d35babad815d7eb6a9fa1e479e9ffbf1210d7c31
itedya <bdomanski@protonmail.com>
2022-08-15 20:52:28 +0200

    Added getIslandAmount into IslandUtil

--- src/main/java/com/itedya/skymaster/utils/IslandUtil.java
+++ src/main/java/com/itedya/skymaster/utils/IslandUtil.java
@@ -1,7 +1,22 @@
 package com.itedya.skymaster.utils;
 
+import com.itedya.skymaster.daos.IslandDao;
+import com.itedya.skymaster.daos.IslandMemberDao;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class IslandUtil {
     public static double getExpandCost(int radius) {
         return Math.pow(radius * 2, 2);
+    }
+
+    public static int getIslandAmount(Connection connection, String playerUuid) throws SQLException {
+        IslandDao islandDao = new IslandDao(connection);
+        IslandMemberDao islandMemberDao = new IslandMemberDao(connection);
+        int ownerSum = islandDao.getSumByOwnerUuid(playerUuid);
+        int memberSum = islandMemberDao.getSumByMemberUuid(playerUuid);
+
+        return ownerSum + memberSum;
     }
 }
