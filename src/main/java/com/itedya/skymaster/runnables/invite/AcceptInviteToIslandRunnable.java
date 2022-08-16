1e86e8fabbc79a4d1475b38d58dd7735b680d334
itedya <bdomanski@protonmail.com>
2022-08-16 12:43:59 +0200

    Added maxAllowedIslands check in AcceptInviteToIslandRunnable

--- src/main/java/com/itedya/skymaster/runnables/invite/AcceptInviteToIslandRunnable.java
+++ src/main/java/com/itedya/skymaster/runnables/invite/AcceptInviteToIslandRunnable.java
@@ -4,48 +4,70 @@ import com.itedya.skymaster.daos.Database;
 import com.itedya.skymaster.daos.IslandMemberDao;
 import com.itedya.skymaster.dtos.database.IslandInviteDto;
 import com.itedya.skymaster.dtos.database.IslandMemberDto;
+import com.itedya.skymaster.runnables.SkymasterRunnable;
+import com.itedya.skymaster.utils.IslandUtil;
+import com.itedya.skymaster.utils.PlayerUtil;
 import com.itedya.skymaster.utils.ThreadUtil;
 import com.itedya.skymaster.utils.WorldGuardUtil;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.chat.ComponentBuilder;
 import org.bukkit.entity.Player;
-import org.bukkit.scheduler.BukkitRunnable;
 
 import java.sql.Connection;
 import java.util.Objects;
 
 // Executor - AcceptInviteToIslandSubCommand
 // Should be started asynchronously
-public class AcceptInviteToIslandRunnable extends BukkitRunnable {
+public class AcceptInviteToIslandRunnable extends SkymasterRunnable {
     private final Player player;
     private final IslandInviteDto dto;
     private Connection connection;
+    private int maxAllowedIslands;
 
     public AcceptInviteToIslandRunnable(Player player, IslandInviteDto dto) {
+        super(player, true);
         this.player = player;
         this.dto = dto;
     }
 
     @Override
     public void run() {
-        var island = dto.islandDto;
-        var toPlayer = dto.toPlayer;
+        maxAllowedIslands = PlayerUtil.getMaxAllowedIslands(dto.toPlayer);
+        ThreadUtil.async(this::validateMaxAllowedIslands);
+    }
 
+    public void validateMaxAllowedIslands() {
         try {
             this.connection = Database.getInstance().getConnection();
 
+            if (IslandUtil.getIslandAmount(connection, dto.toPlayer.getUniqueId().toString()) >= this.maxAllowedIslands) {
+                if (player.getUniqueId().toString().equals(dto.toPlayer.getUniqueId().toString())) {
+                    player.sendMessage(ChatColor.YELLOW + "Nie możesz stworzyć lub dołączyć do tylu wysp! Max: " + maxAllowedIslands);
+                } else {
+                    player.sendMessage(ChatColor.YELLOW + "Ten użytkownik nie może stworzyć lub dołączyć do tylu wysp! Max: " + maxAllowedIslands);
+                }
+
+                return;
+            }
+
+            ThreadUtil.async(this::createIslandMemberDto);
+        } catch (Exception e) {
+            super.errorHandling(e);
+        }
+    }
+
+    public void createIslandMemberDto() {
+        try {
             IslandMemberDto memberDto = new IslandMemberDto();
-            memberDto.islandId = island.id;
-            memberDto.playerUuid = toPlayer.getUniqueId().toString();
+            memberDto.islandId = dto.islandDto.id;
+            memberDto.playerUuid = dto.toPlayer.getUniqueId().toString();
 
             IslandMemberDao islandMemberDao = new IslandMemberDao(connection);
             islandMemberDao.create(memberDto);
 
             ThreadUtil.sync(this::commitWorldGuard);
         } catch (Exception e) {
-            e.printStackTrace();
-            player.sendMessage(ChatColor.RED + "Wystąpił błąd serwera.");
-            this.shutdown();
+            super.errorHandling(e);
         }
     }
 
@@ -98,20 +120,7 @@ public class AcceptInviteToIslandRunnable extends BukkitRunnable {
                     .create());
 
         } catch (Exception e) {
-            e.printStackTrace();
-            player.sendMessage(ChatColor.RED + "Wystąpił błąd serwera.");
-            this.shutdown();
-        }
-    }
-
-    private void shutdown() {
-        try {
-            if (connection != null) {
-                connection.rollback();
-                connection.close();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+            super.errorHandling(e);
         }
     }
 }
