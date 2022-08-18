7753cc0b567823736aa989a491e762575b5f144e
itedya <bdomanski@protonmail.com>
2022-08-19 01:14:35 +0200

    Added format functions into IslandCreationCooldownDto
    - added getStringHours
    - added getStringMinutes
    - added getStringSeconds

--- src/main/java/com/itedya/skymaster/dtos/IslandCreationCooldownDto.java
+++ src/main/java/com/itedya/skymaster/dtos/IslandCreationCooldownDto.java
@@ -4,4 +4,19 @@ package com.itedya.skymaster.dtos;
 public class IslandCreationCooldownDto {
     public String playerUuid;
     public int expiresIn;
+
+    public String getStringHours() {
+        int hours = expiresIn / 3600;
+        return String.format("%02d", hours);
+    }
+
+    public String getStringMinutes() {
+        int minutes = (expiresIn % 3600) / 60;
+        return String.format("%02d", minutes);
+    }
+
+    public String getStringSeconds() {
+        int seconds = expiresIn % 60;
+        return String.format("%02d", seconds);
+    }
 }
